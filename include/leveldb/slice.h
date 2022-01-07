fbcdb0f3a2d6f00d0933535beb90057096d0c7b0
zhouff <sjj050121014@163.com>
2022-01-07 14:00:17 +0800

    finish slice.h

--- include/leveldb/slice.h
+++ include/leveldb/slice.h
@@ -11,6 +11,8 @@
 // external synchronization, but if any of the threads may call a
 // non-const method, all threads accessing the same Slice must use
 // external synchronization.
+// 多线程可以调用const方法而不需要外部同步（锁）
+// 但是如果任意线程访问non-const方法，则所有线程访问这个Slice就需要外部同步（锁）
 
 #ifndef STORAGE_LEVELDB_INCLUDE_SLICE_H_
 #define STORAGE_LEVELDB_INCLUDE_SLICE_H_
@@ -27,44 +29,55 @@ namespace leveldb {
 class LEVELDB_EXPORT Slice {
  public:
   // Create an empty slice.
+  // 创建一个空的slice
   Slice() : data_(""), size_(0) {}
 
   // Create a slice that refers to d[0,n-1].
+  // 创建一个slice，它引用到d[0,n-1]
   Slice(const char* d, size_t n) : data_(d), size_(n) {}
 
   // Create a slice that refers to the contents of "s"
+  // 创建一个slice，它引用"s"的内容
   Slice(const std::string& s) : data_(s.data()), size_(s.size()) {}
 
   // Create a slice that refers to s[0,strlen(s)-1]
+  // 创建一个slice，它引用s[0,strlen(s)-1]
   Slice(const char* s) : data_(s), size_(strlen(s)) {}
 
   // Intentionally copyable.
+  // 有意可拷贝
   Slice(const Slice&) = default;
   Slice& operator=(const Slice&) = default;
 
   // Return a pointer to the beginning of the referenced data
+  // 返回指针，它引用data开始的位置
   const char* data() const { return data_; }
 
   // Return the length (in bytes) of the referenced data
+  // 返回引用data的长度（字节）
   size_t size() const { return size_; }
 
   // Return true iff the length of the referenced data is zero
+  // 如果引用data的长度是0，返回true
   bool empty() const { return size_ == 0; }
 
   // Return the ith byte in the referenced data.
   // REQUIRES: n < size()
+  // 返回引用data的第i个字节
   char operator[](size_t n) const {
     assert(n < size());
     return data_[n];
   }
 
   // Change this slice to refer to an empty array
+  // 改变slice，让它引用一个空数组
   void clear() {
     data_ = "";
     size_ = 0;
   }
 
   // Drop the first "n" bytes from this slice.
+  // 从这个slice丢弃开始的n个字节
   void remove_prefix(size_t n) {
     assert(n <= size());
     data_ += n;
@@ -72,15 +85,18 @@ class LEVELDB_EXPORT Slice {
   }
 
   // Return a string that contains the copy of the referenced data.
+  // 返回一个字符串，它包含引用data的拷贝
   std::string ToString() const { return std::string(data_, size_); }
 
   // Three-way comparison.  Returns value:
+  // 三种方式的比较，返回值如下：
   //   <  0 iff "*this" <  "b",
   //   == 0 iff "*this" == "b",
   //   >  0 iff "*this" >  "b"
   int compare(const Slice& b) const;
 
   // Return true iff "x" is a prefix of "*this"
+  // 如果x是“*this”的前缀，返回true
   bool starts_with(const Slice& x) const {
     return ((size_ >= x.size_) && (memcmp(data_, x.data_, x.size_) == 0));
   }
