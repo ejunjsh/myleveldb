eb53c2b326ac301730820fa4843970017a177fd7
zhouff <sjj050121014@163.com>
2022-01-08 23:28:39 +0800

    finish hash.h

--- util/hash.h
+++ util/hash.h
@@ -3,6 +3,7 @@
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 //
 // Simple hash function used for internal data structures
+// 用于内部数据结构的简单哈希函数
 
 #ifndef STORAGE_LEVELDB_UTIL_HASH_H_
 #define STORAGE_LEVELDB_UTIL_HASH_H_
