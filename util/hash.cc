5afddafb7d0e2b9e0eea949cefa031db6bae7a05
zhouff <sjj050121014@163.com>
2022-01-09 13:54:00 +0800

    finish hash.cc

--- util/hash.cc
+++ util/hash.cc
@@ -11,6 +11,8 @@
 // The FALLTHROUGH_INTENDED macro can be used to annotate implicit fall-through
 // between switch labels. The real definition should be provided externally.
 // This one is a fallback version for unsupported compilers.
+// FALLTHROUGH_INTENDED宏可用于注释switch标签之间的隐式直通。真正的定义应该从外部提供。
+// 这是不支持的编译器的回退版本。
 #ifndef FALLTHROUGH_INTENDED
 #define FALLTHROUGH_INTENDED \
   do {                       \
@@ -21,12 +23,14 @@ namespace leveldb {
 
 uint32_t Hash(const char* data, size_t n, uint32_t seed) {
   // Similar to murmur hash
+  // 类似 murmur hash
   const uint32_t m = 0xc6a4a793;
   const uint32_t r = 24;
   const char* limit = data + n;
   uint32_t h = seed ^ (n * m);
 
   // Pick up four bytes at a time
+  // 一次拿四个字节
   while (data + 4 <= limit) {
     uint32_t w = DecodeFixed32(data);
     data += 4;
@@ -36,6 +40,7 @@ uint32_t Hash(const char* data, size_t n, uint32_t seed) {
   }
 
   // Pick up remaining bytes
+  // 处理剩下字节
   switch (limit - data) {
     case 3:
       h += static_cast<uint8_t>(data[2]) << 16;
