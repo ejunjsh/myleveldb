59583ecaaedf9db4986c093587ea30ca509f1cff
zhouff <sjj050121014@163.com>
2022-01-12 18:10:38 +0800

    finish logging.cc and logging.h

--- util/logging.cc
+++ util/logging.cc
@@ -14,12 +14,14 @@
 
 namespace leveldb {
 
+// 添加num到str
 void AppendNumberTo(std::string* str, uint64_t num) {
   char buf[30];
   std::snprintf(buf, sizeof(buf), "%llu", static_cast<unsigned long long>(num));
   str->append(buf);
 }
 
+// 添加转义字符串
 void AppendEscapedStringTo(std::string* str, const Slice& value) {
   for (size_t i = 0; i < value.size(); i++) {
     char c = value[i];
@@ -34,20 +36,24 @@ void AppendEscapedStringTo(std::string* str, const Slice& value) {
   }
 }
 
+// 数字转字符串
 std::string NumberToString(uint64_t num) {
   std::string r;
   AppendNumberTo(&r, num);
   return r;
 }
 
+// 返回转义字符串
 std::string EscapeString(const Slice& value) {
   std::string r;
   AppendEscapedStringTo(&r, value);
   return r;
 }
 
+// 从in转换成数字放到val
 bool ConsumeDecimalNumber(Slice* in, uint64_t* val) {
   // Constants that will be optimized away.
+  // 将被优化掉的常数
   constexpr const uint64_t kMaxUint64 = std::numeric_limits<uint64_t>::max();
   constexpr const char kLastDigitOfMaxUint64 =
       '0' + static_cast<char>(kMaxUint64 % 10);
@@ -55,6 +61,8 @@ bool ConsumeDecimalNumber(Slice* in, uint64_t* val) {
   uint64_t value = 0;
 
   // reinterpret_cast-ing from char* to uint8_t* to avoid signedness.
+  // 从char*转换到uint8_t* 避免签名？
+
   const uint8_t* start = reinterpret_cast<const uint8_t*>(in->data());
 
   const uint8_t* end = start + in->size();
@@ -65,6 +73,8 @@ bool ConsumeDecimalNumber(Slice* in, uint64_t* val) {
 
     // Overflow check.
     // kMaxUint64 / 10 is also constant and will be optimized away.
+    // 溢出检查
+    // kMaxUint64 / 10 也是个常量，要优化掉
     if (value > kMaxUint64 / 10 ||
         (value == kMaxUint64 / 10 && ch > kLastDigitOfMaxUint64)) {
       return false;
--- util/logging.h
+++ util/logging.h
@@ -4,6 +4,7 @@
 //
 // Must not be included from any .h files to avoid polluting the namespace
 // with macros.
+// 不得包含在任何.h文件，以避免宏污染命名空间。
 
 #ifndef STORAGE_LEVELDB_UTIL_LOGGING_H_
 #define STORAGE_LEVELDB_UTIL_LOGGING_H_
@@ -20,23 +21,32 @@ class Slice;
 class WritableFile;
 
 // Append a human-readable printout of "num" to *str
+// 将“num”的可读打印输出附加到*str
 void AppendNumberTo(std::string* str, uint64_t num);
 
 // Append a human-readable printout of "value" to *str.
 // Escapes any non-printable characters found in "value".
+// 将“value”的可读打印输出附加到*str。
+// 转义在“value”中找到的任何不可打印字符。
 void AppendEscapedStringTo(std::string* str, const Slice& value);
 
 // Return a human-readable printout of "num"
+// 返回“num”的可读打印输出
 std::string NumberToString(uint64_t num);
 
 // Return a human-readable version of "value".
 // Escapes any non-printable characters found in "value".
+// 返回“value”的可读版本。
+//转义在“value”中找到的任何不可打印字符。
 std::string EscapeString(const Slice& value);
 
-// Parse a human-readable number from "*in" into *value.  On success,
+// Parse a human-readable number from "*in" into *val.  On success,
 // advances "*in" past the consumed number and sets "*val" to the
 // numeric value.  Otherwise, returns false and leaves *in in an
 // unspecified state.
+// 将人类可读的数字从“*in”解析为*val.
+// 成功时，在*in中跳过已经消耗的数字，并设置*val为一个数字值
+// 否则，返回false并使*in处于未指定状态。
 bool ConsumeDecimalNumber(Slice* in, uint64_t* val);
 
 }  // namespace leveldb
