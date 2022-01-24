2808947997c4da86e71ac0e0218fff9b25fedaf8
zhouff <sjj050121014@163.com>
2022-01-24 20:28:53 +0800

    finish posix_logger.h

--- util/posix_logger.h
+++ util/posix_logger.h
@@ -4,6 +4,7 @@
 //
 // Logger implementation that can be shared by all environments
 // where enough posix functionality is available.
+// Logger实现，共享给由具有足够posix功能的所有环境使用。
 
 #ifndef STORAGE_LEVELDB_UTIL_POSIX_LOGGER_H_
 #define STORAGE_LEVELDB_UTIL_POSIX_LOGGER_H_
@@ -26,12 +27,16 @@ class PosixLogger final : public Logger {
   // Creates a logger that writes to the given file.
   //
   // The PosixLogger instance takes ownership of the file handle.
+  //创建写入给定文件的logger。
+  //
+  //PosixLogger实例拥有文件句柄的所有权。
   explicit PosixLogger(std::FILE* fp) : fp_(fp) { assert(fp != nullptr); }
 
   ~PosixLogger() override { std::fclose(fp_); }
 
   void Logv(const char* format, std::va_list arguments) override {
     // Record the time as close to the Logv() call as possible.
+    // 记录尽可能靠近Logv()调用的时间。
     struct ::timeval now_timeval;
     ::gettimeofday(&now_timeval, nullptr);
     const std::time_t now_seconds = now_timeval.tv_sec;
@@ -39,6 +44,7 @@ class PosixLogger final : public Logger {
     ::localtime_r(&now_seconds, &now_components);
 
     // Record the thread ID.
+    // 记录线程id
     constexpr const int kMaxThreadIdSize = 32;
     std::ostringstream thread_stream;
     thread_stream << std::this_thread::get_id();
@@ -49,12 +55,14 @@ class PosixLogger final : public Logger {
 
     // We first attempt to print into a stack-allocated buffer. If this attempt
     // fails, we make a second attempt with a dynamically allocated buffer.
+    // 我们首先尝试打印到堆栈分配的缓冲区中。
+    // 如果此尝试失败，我们将使用动态分配的缓冲区进行第二次尝试。
     constexpr const int kStackBufferSize = 512;
     char stack_buffer[kStackBufferSize];
     static_assert(sizeof(stack_buffer) == static_cast<size_t>(kStackBufferSize),
                   "sizeof(char) is expected to be 1 in C++");
 
-    int dynamic_buffer_size = 0;  // Computed in the first iteration.
+    int dynamic_buffer_size = 0;  // Computed in the first iteration. 在第一次迭代中计算。
     for (int iteration = 0; iteration < 2; ++iteration) {
       const int buffer_size =
           (iteration == 0) ? kStackBufferSize : dynamic_buffer_size;
@@ -62,6 +70,7 @@ class PosixLogger final : public Logger {
           (iteration == 0) ? stack_buffer : new char[dynamic_buffer_size];
 
       // Print the header into the buffer.
+      // 将标题打印到缓冲区中。
       int buffer_offset = std::snprintf(
           buffer, buffer_size, "%04d/%02d/%02d-%02d:%02d:%02d.%06d %s ",
           now_components.tm_year + 1900, now_components.tm_mon + 1,
@@ -72,12 +81,14 @@ class PosixLogger final : public Logger {
       // The header can be at most 28 characters (10 date + 15 time +
       // 3 delimiters) plus the thread ID, which should fit comfortably into the
       // static buffer.
+      // 头最多可以是28个字符（10个日期+15个时间+3个分隔符）加上线程ID，该ID应该适合静态缓冲区。
       assert(buffer_offset <= 28 + kMaxThreadIdSize);
       static_assert(28 + kMaxThreadIdSize < kStackBufferSize,
                     "stack-allocated buffer may not fit the message header");
       assert(buffer_offset < buffer_size);
 
       // Print the message into the buffer.
+      // 将消息打印到缓冲区中。
       std::va_list arguments_copy;
       va_copy(arguments_copy, arguments);
       buffer_offset +=
@@ -87,12 +98,16 @@ class PosixLogger final : public Logger {
 
       // The code below may append a newline at the end of the buffer, which
       // requires an extra character.
+      // 下面的代码可能会在缓冲区的末尾追加一个换行符，这需要额外的字符。
       if (buffer_offset >= buffer_size - 1) {
         // The message did not fit into the buffer.
+        // 消息放不下缓冲区。
         if (iteration == 0) {
           // Re-run the loop and use a dynamically-allocated buffer. The buffer
           // will be large enough for the log message, an extra newline and a
           // null terminator.
+          // 重新运行循环并使用动态分配的缓冲区。
+          // 缓冲区将足够大，可以容纳日志消息、一个额外的换行符和一个空终止符。
           dynamic_buffer_size = buffer_offset + 2;
           continue;
         }
@@ -100,11 +115,14 @@ class PosixLogger final : public Logger {
         // The dynamically-allocated buffer was incorrectly sized. This should
         // not happen, assuming a correct implementation of std::(v)snprintf.
         // Fail in tests, recover by truncating the log message in production.
+        // 动态分配的缓冲区大小不正确。假设std::(v)snprintf的正确实现，则不应发生这种情况。
+        // 测试失败，通过截断生产中的日志消息进行恢复。
         assert(false);
         buffer_offset = buffer_size - 1;
       }
 
       // Add a newline if necessary.
+      // 如有必要，添加换行符。
       if (buffer[buffer_offset - 1] != '\n') {
         buffer[buffer_offset] = '\n';
         ++buffer_offset;
