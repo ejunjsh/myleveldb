b97e71a7dc767db404feca543ef2530597187b50
zhouff <sjj050121014@163.com>
2022-01-26 18:52:04 +0800

    finish random.h

--- util/random.h
+++ util/random.h
@@ -12,6 +12,7 @@ namespace leveldb {
 // A very simple random number generator.  Not especially good at
 // generating truly random bits, but good enough for our needs in this
 // package.
+// 一个非常简单的随机数生成器。不是特别擅长生成真正的随机位，但足以满足我们在这个包中的需要。
 class Random {
  private:
   uint32_t seed_;
@@ -19,6 +20,7 @@ class Random {
  public:
   explicit Random(uint32_t s) : seed_(s & 0x7fffffffu) {
     // Avoid bad seeds.
+    // 避免坏的种子
     if (seed_ == 0 || seed_ == 2147483647L) {
       seed_ = 1;
     }
@@ -32,13 +34,18 @@ class Random {
     // seed_ must not be zero or M, or else all subsequent computed values
     // will be zero or M respectively.  For all other values, seed_ will end
     // up cycling through every number in [1,M-1]
+    //
+    // seed_不得为零或M，否则所有后续计算值将分别为零或M。对于所有其他值，seed_最终将遍历[1，M-1]中的每个数字
     uint64_t product = seed_ * A;
 
     // Compute (product % M) using the fact that ((x << 31) % M) == x.
+    // 计算 (product % M)，使用((x << 31) % M) == x这个事实
     seed_ = static_cast<uint32_t>((product >> 31) + (product & M));
     // The first reduction may overflow by 1 bit, so we may need to
     // repeat.  mod == M is not possible; using > allows the faster
     // sign-bit-based test.
+    // 第一次减少可能会溢出1位，因此我们可能需要重复。
+    // mod==M是不可能的；使用>可以更快地进行基于符号位的测试。
     if (seed_ > M) {
       seed_ -= M;
     }
@@ -46,15 +53,21 @@ class Random {
   }
   // Returns a uniformly distributed value in the range [0..n-1]
   // REQUIRES: n > 0
+  // 返回[0..n-1]范围内的均匀分布值
+  // 要求：n>0
   uint32_t Uniform(int n) { return Next() % n; }
 
   // Randomly returns true ~"1/n" of the time, and false otherwise.
   // REQUIRES: n > 0
+  // （这个不知道怎么翻译，调用这个方法有1/n概率是true，是不是这个意思呢？）
+  // 要求：n>0
   bool OneIn(int n) { return (Next() % n) == 0; }
 
   // Skewed: pick "base" uniformly from range [0,max_log] and then
   // return "base" random bits.  The effect is to pick a number in the
   // range [0,2^max_log-1] with exponential bias towards smaller numbers.
+  // Skewed: 从范围[0，max_log]中均匀选取“base”，然后返回“base”随机位。
+  // 其效果是选择[0,2^ max_log-1]范围内的一个数字，并以指数形式偏向较小的数字。
   uint32_t Skewed(int max_log) { return Uniform(1 << Uniform(max_log + 1)); }
 };
 
