bc19e1858fffce73533121d977b620358a2af990
zhouff <sjj050121014@163.com>
2022-02-10 20:25:10 +0800

    finish filter_block.h

--- table/filter_block.h
+++ table/filter_block.h
@@ -5,6 +5,8 @@
 // A filter block is stored near the end of a Table file.  It contains
 // filters (e.g., bloom filters) for all data blocks in the table combined
 // into a single filter block.
+// 过滤器块存储在表文件的末尾附近。
+// 它包含表中所有数据块的过滤器（例如bloom过滤器），这些过滤器块组合成一个更大的过滤器块。
 
 #ifndef STORAGE_LEVELDB_TABLE_FILTER_BLOCK_H_
 #define STORAGE_LEVELDB_TABLE_FILTER_BLOCK_H_
@@ -27,6 +29,10 @@ class FilterPolicy;
 //
 // The sequence of calls to FilterBlockBuilder must match the regexp:
 //      (StartBlock AddKey*)* Finish
+// FilterBlockBuilder用于构造特定表的所有筛选器。它生成一个字符串，作为特殊块存储在表中。
+//
+// 对FilterBlockBuilder的调用顺序必须与regexp匹配：
+//      (StartBlock AddKey*)* Finish
 class FilterBlockBuilder {
  public:
   explicit FilterBlockBuilder(const FilterPolicy*);
@@ -42,25 +48,26 @@ class FilterBlockBuilder {
   void GenerateFilter();
 
   const FilterPolicy* policy_;
-  std::string keys_;             // Flattened key contents
-  std::vector<size_t> start_;    // Starting index in keys_ of each key
-  std::string result_;           // Filter data computed so far
-  std::vector<Slice> tmp_keys_;  // policy_->CreateFilter() argument
+  std::string keys_;             // Flattened key contents 扁平化键内容
+  std::vector<size_t> start_;    // Starting index in keys_ of each key 在每个键的key_中开始索引
+  std::string result_;           // Filter data computed so far 筛选到目前为止计算的数据
+  std::vector<Slice> tmp_keys_;  // policy_->CreateFilter() argument 参数
   std::vector<uint32_t> filter_offsets_;
 };
 
 class FilterBlockReader {
  public:
   // REQUIRES: "contents" and *policy must stay live while *this is live.
+  // 要求："contents" 和 *policy必须有效
   FilterBlockReader(const FilterPolicy* policy, const Slice& contents);
   bool KeyMayMatch(uint64_t block_offset, const Slice& key);
 
  private:
   const FilterPolicy* policy_;
-  const char* data_;    // Pointer to filter data (at block-start)
-  const char* offset_;  // Pointer to beginning of offset array (at block-end)
-  size_t num_;          // Number of entries in offset array
-  size_t base_lg_;      // Encoding parameter (see kFilterBaseLg in .cc file)
+  const char* data_;    // Pointer to filter data (at block-start) 指向筛选器数据（在块开始位置）
+  const char* offset_;  // Pointer to beginning of offset array (at block-end) 指向偏移数组的开始位置（块结束位置）
+  size_t num_;          // Number of entries in offset array 偏移数组的条目数
+  size_t base_lg_;      // Encoding parameter (see kFilterBaseLg in .cc file) 编码参数（详情看相应的.cc文件里面的kFilterBaseLg）
 };
 
 }  // namespace leveldb
