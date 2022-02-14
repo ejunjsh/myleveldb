ebb3b688d60c3661fdc14a92657aa5195fd7055f
zhouff <sjj050121014@163.com>
2022-02-14 22:58:53 +0800

    finish filter_block.cc

--- table/filter_block.cc
+++ table/filter_block.cc
@@ -10,8 +10,10 @@
 namespace leveldb {
 
 // See doc/table_format.md for an explanation of the filter block format.
+// 查看doc/table_format.md去理解筛选器块的格式
 
 // Generate new filter every 2KB of data
+// 每2KB数据生成一个新的筛选器
 static const size_t kFilterBaseLg = 11;
 static const size_t kFilterBase = 1 << kFilterBaseLg;
 
@@ -38,13 +40,14 @@ Slice FilterBlockBuilder::Finish() {
   }
 
   // Append array of per-filter offsets
+  // 给每个筛选器的偏移量添加到偏移量数组
   const uint32_t array_offset = result_.size();
   for (size_t i = 0; i < filter_offsets_.size(); i++) {
     PutFixed32(&result_, filter_offsets_[i]);
   }
 
   PutFixed32(&result_, array_offset);
-  result_.push_back(kFilterBaseLg);  // Save encoding parameter in result
+  result_.push_back(kFilterBaseLg);  // Save encoding parameter in result 保存编码参数到结果里面
   return Slice(result_);
 }
 
@@ -52,12 +55,14 @@ void FilterBlockBuilder::GenerateFilter() {
   const size_t num_keys = start_.size();
   if (num_keys == 0) {
     // Fast path if there are no keys for this filter
+    // 如果没有键给这个筛选器，快速返回
     filter_offsets_.push_back(result_.size());
     return;
   }
 
   // Make list of keys from flattened key structure
-  start_.push_back(keys_.size());  // Simplify length computation
+  // 把扁平化的键结构转化成列表（tmp_keys_）
+  start_.push_back(keys_.size());  // Simplify length computation 简化长度计算
   tmp_keys_.resize(num_keys);
   for (size_t i = 0; i < num_keys; i++) {
     const char* base = keys_.data() + start_[i];
@@ -66,6 +71,7 @@ void FilterBlockBuilder::GenerateFilter() {
   }
 
   // Generate filter for current set of keys and append to result_.
+  // 为当前的键集合生成筛选器，并附加到result_
   filter_offsets_.push_back(result_.size());
   policy_->CreateFilter(&tmp_keys_[0], static_cast<int>(num_keys), &result_);
 
@@ -78,7 +84,7 @@ FilterBlockReader::FilterBlockReader(const FilterPolicy* policy,
                                      const Slice& contents)
     : policy_(policy), data_(nullptr), offset_(nullptr), num_(0), base_lg_(0) {
   size_t n = contents.size();
-  if (n < 5) return;  // 1 byte for base_lg_ and 4 for start of offset array
+  if (n < 5) return;  // 1 byte for base_lg_ and 4 for start of offset array 1个字节是base_lg_ , 4个字节是放偏移量数组的开始位置
   base_lg_ = contents[n - 1];
   uint32_t last_word = DecodeFixed32(contents.data() + n - 5);
   if (last_word > n - 5) return;
@@ -97,10 +103,11 @@ bool FilterBlockReader::KeyMayMatch(uint64_t block_offset, const Slice& key) {
       return policy_->KeyMayMatch(key, filter);
     } else if (start == limit) {
       // Empty filters do not match any keys
+      // 空的筛选器不匹配任何键
       return false;
     }
   }
-  return true;  // Errors are treated as potential matches
+  return true;  // Errors are treated as potential matches 错误视为潜在匹配
 }
 
 }  // namespace leveldb
