384c6b16a19413beb5b637f7f964864ea74d9e66
zhouff <sjj050121014@163.com>
2022-03-08 16:53:23 +0800

    finish log_writer.h

--- db/log_writer.h
+++ db/log_writer.h
@@ -22,11 +22,17 @@ class Writer {
   // Create a writer that will append data to "*dest".
   // "*dest" must be initially empty.
   // "*dest" must remain live while this Writer is in use.
+  // 创建一个写者，将数据附加到“*dest”。
+  // “*dest”最初必须为空。
+  // “*dest”在这个写者使用期间必须保持活动状态。
   explicit Writer(WritableFile* dest);
 
   // Create a writer that will append data to "*dest".
   // "*dest" must have initial length "dest_length".
   // "*dest" must remain live while this Writer is in use.
+  // 创建一个写者，将数据附加到“*dest”。
+  // “*dest”最初必须为空。
+  // “*dest”在这个写者使用期间必须保持活动状态。
   Writer(WritableFile* dest, uint64_t dest_length);
 
   Writer(const Writer&) = delete;
@@ -40,11 +46,12 @@ class Writer {
   Status EmitPhysicalRecord(RecordType type, const char* ptr, size_t length);
 
   WritableFile* dest_;
-  int block_offset_;  // Current offset in block
+  int block_offset_;  // Current offset in block 在块中当前的偏移量
 
   // crc32c values for all supported record types.  These are
   // pre-computed to reduce the overhead of computing the crc of the
   // record type stored in the header.
+  // 所有支持的记录类型的crc32c值。这些是预先计算的，以减少计算存储在报头中的记录类型的crc的开销。
   uint32_t type_crc_[kMaxRecordType + 1];
 };
 
