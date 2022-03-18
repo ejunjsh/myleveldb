d93a41ca587a6d6e11f7ead3f6589f04e7638d4b
zhouff <sjj050121014@163.com>
2022-03-18 16:16:54 +0800

    finish memtable.h

--- db/memtable.h
+++ db/memtable.h
@@ -21,15 +21,18 @@ class MemTable {
  public:
   // MemTables are reference counted.  The initial reference count
   // is zero and the caller must call Ref() at least once.
+  // MemTables是引用计数的。初始引用计数为零，调用方必须至少调用Ref()一次。
   explicit MemTable(const InternalKeyComparator& comparator);
 
   MemTable(const MemTable&) = delete;
   MemTable& operator=(const MemTable&) = delete;
 
   // Increase reference count.
+  // 增加引用计数
   void Ref() { ++refs_; }
 
   // Drop reference count.  Delete if no more references exist.
+  // 删除引用计数。如果不再存在引用，请删除。
   void Unref() {
     --refs_;
     assert(refs_ >= 0);
@@ -40,6 +43,7 @@ class MemTable {
 
   // Returns an estimate of the number of bytes of data in use by this
   // data structure. It is safe to call when MemTable is being modified.
+  // 返回此数据结构使用的数据字节数的估计值。在修改MemTable时调用是安全的。
   size_t ApproximateMemoryUsage();
 
   // Return an iterator that yields the contents of the memtable.
@@ -48,11 +52,17 @@ class MemTable {
   // while the returned iterator is live.  The keys returned by this
   // iterator are internal keys encoded by AppendInternalKey in the
   // db/format.{h,cc} module.
+  // 返回生成memtable内容的迭代器。
+  //
+  // 调用方必须确保在返回的迭代器处于活动状态时，基础MemTable保持活动状态。
+  // 该迭代器返回的键是由在db/format.{h,cc}模块里面的AppendInternalKey编码的内部键
   Iterator* NewIterator();
 
   // Add an entry into memtable that maps key to value at the
   // specified sequence number and with the specified type.
   // Typically value will be empty if type==kTypeDeletion.
+  // 在memtable中添加一个条目，该条目将键映射到指定序列号和指定类型的值。
+  // 如果type==kTypeDelete，则该值通常为空。
   void Add(SequenceNumber seq, ValueType type, const Slice& key,
            const Slice& value);
 
@@ -60,6 +70,9 @@ class MemTable {
   // If memtable contains a deletion for key, store a NotFound() error
   // in *status and return true.
   // Else, return false.
+  // 如果memtable包含key的值，则将其存储在*value中并返回true。
+  // 如果memtable包含对键的删除，则将NotFound（）错误存储在*status中，并返回true。
+  // 否则，返回false。
   bool Get(const LookupKey& key, std::string* value, Status* s);
 
  private:
@@ -75,7 +88,7 @@ class MemTable {
   typedef SkipList<const char*, KeyComparator> Table;
 
   ~MemTable();  // Private since only Unref() should be used to delete it
-
+                // 这里的析构函数是私有，这样外部不能delete它，只能调用Unref()来删除它
   KeyComparator comparator_;
   int refs_;
   Arena arena_;
