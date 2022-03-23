55d1d30950e3dd476771ad7675a81d7ec2c4ebba
zhouff <sjj050121014@163.com>
2022-03-23 12:08:20 +0800

    finish table_cache.h

--- db/table_cache.h
+++ db/table_cache.h
@@ -3,6 +3,7 @@
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 //
 // Thread-safe (provides internal synchronization)
+// 线程安全（提供内部同步）
 
 #ifndef STORAGE_LEVELDB_DB_TABLE_CACHE_H_
 #define STORAGE_LEVELDB_DB_TABLE_CACHE_H_
@@ -35,16 +36,24 @@ class TableCache {
   // underlies the returned iterator.  The returned "*tableptr" object is owned
   // by the cache and should not be deleted, and is valid for as long as the
   // returned iterator is live.
+  // 返回指定文件号的迭代器（对应的文件长度必须等于"file_size"）
+  // 如果"tableptr" 非空，那设置"*tableptr"指向返回迭代器的底层表对象
+  // 如果返回迭代器的底层表对象没有，则设置"*tableptr"指向nullptr,
+  // 这个返回的"*tableptr" 对象，是由这个缓存所有，不应该被删除
+  // 只要返回的迭代器活着，则这个表对象就一直有效
   Iterator* NewIterator(const ReadOptions& options, uint64_t file_number,
                         uint64_t file_size, Table** tableptr = nullptr);
 
   // If a seek to internal key "k" in specified file finds an entry,
   // call (*handle_result)(arg, found_key, found_value).
+  // 如果能找到内部键“k”相对应文件里的一个条目，
+  // 则调用(*handle_result)(arg, found_key, found_value).
   Status Get(const ReadOptions& options, uint64_t file_number,
              uint64_t file_size, const Slice& k, void* arg,
              void (*handle_result)(void*, const Slice&, const Slice&));
 
   // Evict any entry for the specified file number
+  // 逐出指定文件号的任何条目
   void Evict(uint64_t file_number);
 
  private:
